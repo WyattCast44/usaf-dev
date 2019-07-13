28b6f8d5206223774da576aaad17eab637680367
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2019-07-13 12:38:02 -0600

    wip

--- app/Models/Passport/Client.php
+++ app/Models/Passport/Client.php
@@ -6,5 +6,13 @@ use Laravel\Passport\Client as BaseClient;
 
 class Client extends BaseClient
 {
-    //
+    /**
+    * Determine if the client should skip the authorization prompt.
+    *
+    * @return bool
+    */
+    public function skipsAuthorization()
+    {
+        return $this->first_party;
+    }
 }
