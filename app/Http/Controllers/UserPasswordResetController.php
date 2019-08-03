60d87a13667a01584aef709f3286de0668286191
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2019-08-03 08:13:07 -0600

    wip

--- app/Http/Controllers/UserPasswordResetController.php
+++ app/Http/Controllers/UserPasswordResetController.php
@@ -4,25 +4,33 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Rules\PasswordMinLength;
+use Illuminate\Support\Facades\Hash;
 
 class UserPasswordResetController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth', 'verified']);
+    }
+
     public function update(Request $request)
     {
+        $user = auth()->user();
+        
+        if (!Hash::check($request->password, $user->password)) {
+            alert('Unable to update password', 'The password entered did not match your current password, please try again.', 'warning');
+
+            return back();
+        }
+        
         $this->validate($request, [
             'password' => 'required|string',
             'new_password' => ['required', 'string', 'confirmed', new PasswordMinLength]
         ]);
 
-        $user = auth()->user();
+        $user->update(['password' => bcrypt($request->new_password)]);
 
-        if ($user->password <> bcrypt($request->password)) {
-            alert('Unable to update password', 'The password entered did not match your current password, please try again.', 'warning');
-
-            return back();
-        }
-
-        alert('Password Updated!', 'success');
+        alert('Password Updated!', '', 'success');
 
         return back();
     }
